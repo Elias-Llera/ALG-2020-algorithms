ef63fac10107b1d8cbb332b75bc50d903b56eb80
Elías <Elas@DESKTOP-56CGTGQ>
2020-02-02 17:56:41 +0100

    Last push I forgot to edit the matrix constructor to have numbers only in range [1, 4]

--- src/main/java/session1_1/MatrixOperations.java
+++ src/main/java/session1_1/MatrixOperations.java
@@ -8,7 +8,7 @@ public class MatrixOperations {
 		matrix = new int[n][n];
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[i].length; j++) {
-				matrix[i][j] = (int) (Math.random() * 10);
+				matrix[i][j] = (int) (Math.random() * 3) + 1;
 			}
 		}
 	}
